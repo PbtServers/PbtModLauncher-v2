3c4149a1e5db66485c3431eb1322b9072974b8e0
Pbt <68289322+PbtServers@users.noreply.github.com>
2023-05-31 15:07:24 +0200

    Revisar Hash para Futuras Actualizaciones de Java

---
+++ scripts/CheckHash.ps1
@@ -0,0 +1,10 @@
+$uri = 'https://cdn.azul.com/zulu/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_x64.tar.gz'
+$name = 'zulu8.70.0.23-ca-jdk8.0.372-linux_x64'
+$req = Invoke-WebRequest $uri
+
+# get the remote file hash
+$remoteHash = Get-FileHash -InputStream $req.RawContentStream -Algorithm SHA256
+
+Write-Host $remoteHash
+
+echo $remoteHash > $name.hash.txt
